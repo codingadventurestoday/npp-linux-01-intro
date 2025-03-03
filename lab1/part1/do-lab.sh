a702b9a5956281ff49a4f569db70d8a1a76a3f0d
JLMP <codingadventures@gmail.com>
2025-03-03 20:35:37 +0000

    manually used commands instead of aliases file. Plus download scapy

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -2,4 +2,16 @@
 
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
+alias sw="docker exec -it clab-lab1-part1-switch"
+alias host1="docker exec -it clab-lab1-part1-host1"
+alias host2="docker exec -it clab-lab1-part1-host2"
+alias host3="docker exec -it clab-lab1-part1-host3"
+alias host4="docker exec -it clab-lab1-part1-host4"
 
+docker exec -it clab-lab1-part1-switch ip link add name mybridge type bridge
+docker exec -it clab-lab1-part1-switch ip link set mybridge up 
+
+docker exec -it clab-lab1-part1-switch ip link set eth1 master mybridge 
+docker exec -it clab-lab1-part1-switch ip link set eth2 master mybridge
+docker exec -it clab-lab1-part1-switch ip link set eth3 master mybridge
+docker exec -it clab-lab1-part1-switch ip link set eth4 master mybridge 
